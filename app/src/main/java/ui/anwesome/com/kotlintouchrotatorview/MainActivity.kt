c4ecc4144aa979baccfb7141f32c9afcecbc08a1
Anwesh43 <anweshthecool0@gmail.com>
2018-01-10 08:17:52 +0530

    Using OnRotateStopListener in Activity

--- app/src/main/java/ui/anwesome/com/kotlintouchrotatorview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlintouchrotatorview/MainActivity.kt
@@ -2,12 +2,16 @@ package ui.anwesome.com.kotlintouchrotatorview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ui.anwesome.com.touchrotatorview.TouchRotatorView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        TouchRotatorView.create(this)
+        var view = TouchRotatorView.create(this)
+        view.addOnRotateStopListener {
+            Toast.makeText(this,"Line has stopped",Toast.LENGTH_SHORT).show()
+        }
     }
 }
